f02f5405aa76a9b5dc61c6b6254ab70eb9793f13
“Cade <caderosche@gmail.com>
2023-10-04 12:23:39 -0500

    copied in code

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-co-op/gocron"
+)
+
+func task() {
+	fmt.Println("I am a task")
+}
+
 func main() {
-	println("Hello, World!")
+	// Create a new scheduler
+	s := gocron.NewScheduler(time.UTC)
+
+	// Schedule a task to run every second
+	s.Every(1).Second().Do(task)
+
+	// Schedule a task to run at
+	// 10:30 every day
+	s.Every(1).Day().At("10:30").Do(task)
+
+	// Start the scheduler
+	s.StartBlocking()
 }
